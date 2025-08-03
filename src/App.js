621a3a7c88acbe2057a15cedffa941d5d6cf7f74
DrChilliPepper <shuchu2004@gmail.com>
2025-08-03 22:25:22 +0530

    Can't work with JSON for hosting

--- src/App.js
+++ src/App.js
@@ -4,90 +4,38 @@ import Footer from './Footer';
 import { useState, useEffect} from 'react';
 import AddItems from './AddItems';
 import SearchItem from './SearchItem';
-import apiRequest from './apiRequest';
 
 function App() {
-  const API_URL = 'https://api.myjson.online/v1/records/d9f5a435-ac34-4bb4-8199-e8685a73d3e4';
 
-  const [items, setitems] = useState([]);
+  const [items, setitems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
   const [newitem, setnewitem] = useState('')
   const [search, setsearch] = useState('')
-  const [fetcherror, setfetcherror] = useState(null);
-  const [isloading, setisloading] = useState(true);
 
   useEffect(() => {
-    const fetchitems = async () => {
-      try{
-        const response = await fetch(API_URL);
-        if(!response.ok) throw Error('Did not recieve expected data');
-        const listitems = await response.json();
-        console.log(listitems);
-        setitems(listitems);
-        setfetcherror(null);
-      }
-      catch (err){
-        console.log(err.stack)
-        setfetcherror(err.message);
-      } finally {
-        setisloading(false);
-      }
-    }
+    localStorage.setItem('shoppinglist', JSON.stringify(items));
+  }, [items])
 
-    (async () => await fetchitems())();
-  }, [])
-
-  const additem = async (item) => {
-    const id = items.length ? items[items.length-1].id + 1 : 1
-    const mynewitem = {
-      id,
-      checked: false,
-      item
-    };
-    const listitems = [...items, mynewitem];
-    setitems(listitems)
-    
-    const postoptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type' : 'application/json'
-      },
-      body: JSON.stringify(mynewitem)
-    }
-    const result = await apiRequest(API_URL, postoptions);
-    if (result) setfetcherror(result);
+   const additems = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setitems(listItems);
   }
 
-  const handlecheck = async (id) => {
-    const listitems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    setitems(listitems);
-
-    const myitem = listitems.filter((item) => item.id === id);
-    const updateoptions = {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({checked: myitem[0].checked})
-    };
-    const reqUrl = `${API_URL}/${id}`;
-    const result = await apiRequest(reqUrl, updateoptions);
-    if (result) setfetcherror(result);
+  const handlecheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setitems(listItems);
   }
 
-  const handledelete = async (id) => {
-    const listitems = items.filter((item) => item.id !== id);
-    setitems(listitems);
-
-    const deleteoptions = {method: 'DELETE'};
-    const reqUrl = `${API_URL}/${id}`;
-    const result = await apiRequest(reqUrl, deleteoptions);
-    if (result) setfetcherror(result);
+  const handledelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setitems(listItems);
   }
 
   const handlesubmit = (e) => {
     e.preventDefault();
     if (!newitem) return;
-    additem(newitem);
+    additems(newitem);
     setnewitem('')
   }
 
@@ -103,15 +51,11 @@ function App() {
       search={search}
       setsearch={setsearch}
       />
-      <main>
-        {isloading && <p>Loading items...</p>}
-        {fetcherror && <p style={{color: "red"}}>{`Error: ${fetcherror}`}</p>}
-        {!fetcherror && !isloading && <Content
+      <Content
         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handlecheck={handlecheck}
         handledelete={handledelete}
-        />}
-      </main>
+        />
       <Footer
       length={items.length}
       />
